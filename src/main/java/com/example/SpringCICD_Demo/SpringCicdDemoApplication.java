eea0506168682b5adec042c3d3e2ab2b716b181e
Asif-7842714544 <casif989@gmail.com>
2024-05-05 13:15:52 +0530

    CICD_Test

---
+++ src/main/java/com/example/SpringCICD_Demo/SpringCicdDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringCICD_Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCicdDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCicdDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringCICD_Demo/SpringCicdDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringCICD_Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCicdDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
