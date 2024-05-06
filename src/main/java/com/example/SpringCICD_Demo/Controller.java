f677475866f58437cb1c15b4dd622b1346ef09d4
Asif-7842714544 <casif989@gmail.com>
2024-05-06 10:16:28 +0530

    adding lombok

--- src/main/java/com/example/SpringCICD_Demo/Controller.java
+++ src/main/java/com/example/SpringCICD_Demo/Controller.java
@@ -1,20 +1,24 @@
 package com.example.SpringCICD_Demo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
+@Slf4j
 public class Controller {
 
     @GetMapping("/msg")
-    public String getMessage(){
+    public String getMessage() {
+        log.info("inside get message");
         return "Hello World!";
     }
 
     @GetMapping("/msg/Asif")
-    public String getNewMessage(){
+    public String getNewMessage() {
+        log.info("inside get new message");
         return "Hello World! Asif";
     }
 }
